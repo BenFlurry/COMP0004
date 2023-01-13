7d479d6b117cb9c6ccc8f5e2ce975af4ebbfee78
Ben Alexander <benjaminalexander747@gmail.com>
2023-01-13 01:06:09 +0000

    All core (that are acc fun) done

--- Week1/src/Ex16.java
+++ Week1/src/Ex16.java
@@ -1,2 +1,16 @@
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Ex16 {
+    public static void main(String[] args) {
+        new Ex16().same_chars("hello".toCharArray(), "hello1".toCharArray());
+    }
+
+    public void same_chars(char[] c1, char[] c2) {
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(Arrays.equals(c1, c2));
+    }
 }
